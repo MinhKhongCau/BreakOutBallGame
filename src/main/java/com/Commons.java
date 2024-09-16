719cbfe7d28f8ae032c29d4565c04eea00a4c933
MinhKhongCau <minhngoquang6@gmail.com>
2024-09-16 19:31:17 +0700

    adđ new project

---
+++ src/main/java/com/Commons.java
@@ -0,0 +1,13 @@
+package com;
+
+public interface Commons {
+    public int WIDTH = 300;
+    public int HEIGHT = 400;
+    public int BOTTOM_EDGE = 390;
+    public int N_OF_BRICKS = 30;
+    public int INIT_PADDLE_X = 200;
+    public int INIT_PADDLE_Y = 360;
+    public int INIT_BALL_X = 230;
+    public int INIT_BALL_Y = 355;
+    public int PERIOD = 10;
+}
---
+++ src/main/java/com/Sprite.java
@@ -0,0 +1,64 @@
+package com;
+
+import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+
+public class Sprite {
+    public int x;
+    public int y;
+    public int imageWidth;
+    public int imageHeight;
+    public BufferedImage image;
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+    public int getImageWidth() {
+        return imageWidth;
+    }
+
+    public void setImageWidth(int imageWidth) {
+        this.imageWidth = imageWidth;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    Rectangle getRect() {
+
+        return new Rectangle(x, y,
+                image.getWidth(null), image.getHeight(null));
+    }
+
+    void getImageDimensions() {
+
+        imageWidth = image.getWidth(null);
+        imageHeight = image.getHeight(null);
+    }
+}
