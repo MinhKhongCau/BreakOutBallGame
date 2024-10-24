d94b9006490a08f9e1cd9b7c300524f8df9f136b
biin284 <anhn162004@gmail.com>
2024-10-24 16:31:21 +0700

    Message

--- src/main/java/com/Ball.java
+++ src/main/java/com/Ball.java
@@ -4,18 +4,21 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class Ball extends Sprite{
+public class Ball extends Sprite {
     private int width;
     private int height;
-    private int dir=1;
+    private int dir = 1;
     private int speed;
-    
-    public Ball () {
+
+    protected int dx; // vận tốc theo trục X
+    protected int dy; // vận tốc theo trục Y
+
+    public Ball() {
         this.x = Commons.INIT_BALL_X;
         this.y = Commons.INIT_BALL_Y;
         this.width = Commons.BALL_SIZE;
         this.height = Commons.BALL_SIZE;
-        this.speed = Commons.BALL_SPEED-400;
+        this.speed = Commons.BALL_SPEED - 400;
     }
 
     public Ball(int width, int height) {
@@ -40,27 +43,26 @@ public class Ball extends Sprite{
     public void setHeight(int height) {
         this.height = height;
     }
-   
+
     public void setDir(int dir) {
-    	this.dir=dir;
+        this.dir = dir;
     }
-    
+
     public void setSpeed(int speed) {
-    	this.speed = speed;
+        this.speed = speed;
     }
-    
-    public void move () {
+
+    public void move() {
         try {
-            this.x += (int) dir*speed*Commons.DELTA_TIME;
-            this.y += (int) dir*speed*Commons.DELTA_TIME;
-            
-            
+            this.x += (int) dir * speed * Commons.DELTA_TIME;
+            this.y += (int) dir * speed * Commons.DELTA_TIME;
+
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
     }
-    
-    public void draw (Graphics g) {
+
+    public void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.WHITE);
         g2.fillOval(this.x, this.y, this.width, this.height);
